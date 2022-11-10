19d5e0740f95cecd1366c707e771421831b45b3f
Krisstinkin <krisstinkin@mail.ru>
2022-11-10 22:33:43 +0300

    аыва

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
-console.log('Привет, мир!');
+console.log("Привет, мир!")
+
+function updateInput(book) {
+    document.getElementById("bookNameUpdate").value = book.title
+    document.getElementById("bookAuthorUpdate").value = book.authors
+    document.getElementById("bookYearUpdate").value = book.year
+    document.getElementById("bookImageUpdate").value = book.image
+}
